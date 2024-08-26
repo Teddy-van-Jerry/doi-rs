93516710581e39418bf9ca5c4548a126d00d79af
Teddy van Jerry <me@teddy-van-jerry.org>
2024-08-26 15:44:36 +0800

    Update Documentation for v0.2.0

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # DOI for Rust
 Digital Object Identifier (DOI) resolver for Rust
 
+This crate provides a simple way to resolve DOIs using the [DOI](https://www.doi.org) APIs.
+
 ## Synchronous Design
 This library is designed to be *synchronous*,
 depending on the [`ureq` library](https://docs.rs/ureq) for HTTP requests.
+
+## Usage
+The following is a basic example of using this library:
+```rust
+use doi::Doi;
+let doi = Doi::new("10.1109/TCSII.2024.3366282");
+match doi.resolve() {
+    Ok(resolved) => println!("Resolved Link: {}", resolved),
+    Err(e) => eprintln!("Error: {}", e),
+}
+```
+Please refer to the API documentation for more information.
+More complicated constructions can be done using the `DoiBuilder` struct.
 
 ## License
 This project is licensed under the [MIT license](LICENSE).
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
-/// Digital Object Identifier (DOI) resolver for Rust
+//! Digital Object Identifier (DOI) resolver for Rust
+//!
+//! This crate provides a simple way to resolve DOIs using the [DOI](https://www.doi.org) APIs.
+//!
+//! ## Synchronous Design
+//! This library is designed to be *synchronous*,
+//! depending on the [`ureq` library](https://docs.rs/ureq) for HTTP requests.
+//!
+//! ## Usage
+//! The following is a basic example of using this library:
+//! ```rust
+//! use doi::Doi;
+//! let doi = Doi::new("10.1109/TCSII.2024.3366282");
+//! match doi.resolve() {
+//!     Ok(resolved) => println!("Resolved Link: {}", resolved),
+//!     Err(e) => eprintln!("Error: {}", e),
+//! }
+//! ```
+//!
+//! Please refer to the API documentation for more information.
+//! More complicated constructions can be done using the [`DoiBuilder`] struct.
+//!
+//! ## License
+//! This project is licensed under the [MIT license](https://github.com/Teddy-van-Jerry/doi-rs/blob/master/LICENSE).
+
 extern crate ureq;
 use std::error::Error;
 use ureq::Agent;
@@ -156,6 +180,7 @@ impl PartialEq for Doi {
     }
 }
 
+/// Builder for the [`Doi`] struct.
 #[derive(Debug, Clone, Default)]
 pub struct DoiBuilder {
     /// An `Option<String>` representing the DOI number.
@@ -166,7 +191,6 @@ pub struct DoiBuilder {
     proxy: Option<ureq::Proxy>,
 }
 
-/// Builder for the [`Doi`] struct.
 impl DoiBuilder {
     /// Creates a new instance of [`DoiBuilder`].
     ///
@@ -253,9 +277,9 @@ impl DoiBuilder {
     }
 
     /// Builds the [`Doi`] instance.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// use doi::{Doi, DoiBuilder};
     /// let doi = DoiBuilder::new().doi("10.1109/TCSII.2024.3366282").build();
